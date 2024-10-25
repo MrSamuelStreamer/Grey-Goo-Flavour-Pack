f80a045cbce07300e8c9a7c21de9bcf3aa54ca81
Kieran David Evans <keyz182@gmail.com>
2024-10-25 21:52:51 +0100

    New shambler incident that spawns a single shambler 10 times a day

---
+++ 1.5/Source/Grey_Goo/IncidentWorker_GooShamblerSwarm.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Grey_Goo;
+
+public class IncidentWorker_GooShamblerSwarm: IncidentWorker_ShamblerSwarm
+{
+    protected override List<Pawn> GenerateEntities(IncidentParms parms, float points)
+    {
+        return [base.GenerateEntities(parms, points).RandomElement()];
+    }
+}
