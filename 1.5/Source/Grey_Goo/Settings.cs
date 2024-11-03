6766db54a0f6b264f87c827acf0b0bab2c68aa84
Kieran David Evans <keyz182@gmail.com>
2024-11-03 13:58:28 +0000

    Add scroll to settings

--- 1.5/Source/Grey_Goo/Settings.cs
+++ 1.5/Source/Grey_Goo/Settings.cs
@@ -15,6 +15,7 @@ public class Settings : ModSettings
     public float ChanceForGooToDamagePercent = 0.001f;
     public FloatRange GooDamageRange = new FloatRange(0f,4f);
     public bool InfectOnGooTouch = false;
+    public Vector2 scrollPosition = Vector2.zero;
 
     public int MaxShamblersOnMap = 40;
 
@@ -23,53 +24,67 @@ public class Settings : ModSettings
 
     public void DoWindowContents(Rect wrect)
     {
-        var options = new Listing_Standard();
-        options.Begin(wrect);
+        float scrollViewHeigh = 650f;
 
-        options.CheckboxLabeled("MSS_GG_Setting_EnableGoo".Translate(), ref EnableGoo);
-        options.Gap();
+        Rect viewRect = new Rect(0f, 0f, wrect.width- 20, scrollViewHeigh);
+        scrollPosition = GUI.BeginScrollView(new Rect(0, 50, wrect.width, wrect.height - 50), scrollPosition, viewRect);
 
-        WorldMapGooIncrementPercentPerTick = Widgets.HorizontalSlider(options.GetRect(40f), WorldMapGooIncrementPercentPerTick, 0f, 0.01f, label: "MSS_GG_Setting_GooSpreadIncrement".Translate(WorldMapGooIncrementPercentPerTick.ToString("0.000")));
-        options.Gap();
+        Listing_Standard options = new Listing_Standard();
 
-        GooSpreadChance = Widgets.HorizontalSlider(options.GetRect(40f), GooSpreadChance, 0f, 10f, label: "MSS_GG_Setting_GooSpreadChance".Translate(GooSpreadChance.ToString("0.000")));
-        options.Gap();
+        options.Begin(viewRect);
+        try
+        {
+            options.CheckboxLabeled("MSS_GG_Setting_EnableGoo".Translate(), ref EnableGoo);
+            options.Gap();
 
-        options.Label("MSS_GG_Setting_MapGooUpdateFrequency".Translate(MapGooUpdateFrequency));
-        options.IntAdjuster(ref MapGooUpdateFrequency, 1);
+            WorldMapGooIncrementPercentPerTick = Widgets.HorizontalSlider(options.GetRect(40f), WorldMapGooIncrementPercentPerTick, 0f, 0.01f,
+                label: "MSS_GG_Setting_GooSpreadIncrement".Translate(WorldMapGooIncrementPercentPerTick.ToString("0.000")));
+            options.Gap();
 
-        options.Gap();
+            GooSpreadChance = Widgets.HorizontalSlider(options.GetRect(40f), GooSpreadChance, 0f, 10f,
+                label: "MSS_GG_Setting_GooSpreadChance".Translate(GooSpreadChance.ToString("0.000")));
+            options.Gap();
 
-        ChanceToSpreadGooToCell = Widgets.HorizontalSlider(options.GetRect(40f), ChanceToSpreadGooToCell, 0f, 0.25f, label: "MSS_GG_Setting_ChanceToSpreadGooToCell".Translate(ChanceToSpreadGooToCell.ToString("0.000")));
-        options.Gap();
+            options.Label("MSS_GG_Setting_MapGooUpdateFrequency".Translate(MapGooUpdateFrequency));
+            options.IntAdjuster(ref MapGooUpdateFrequency, 1);
 
-        ChanceForGooToDamagePercent = Widgets.HorizontalSlider(options.GetRect(40f), ChanceForGooToDamagePercent, 0f, 0.1f, label: "MSS_GG_Setting_ChanceForGooToDamagePercent".Translate(ChanceForGooToDamagePercent.ToString("0.000")));
-        options.Gap();
+            options.Gap();
 
-        Widgets.FloatRange(options.GetRect(40), 1, ref GooDamageRange, 0f, 10f, "MSS_GG_Setting_GooDamageRange");
-        options.Gap();
+            ChanceToSpreadGooToCell = Widgets.HorizontalSlider(options.GetRect(40f), ChanceToSpreadGooToCell, 0f, 0.25f,
+                label: "MSS_GG_Setting_ChanceToSpreadGooToCell".Translate(ChanceToSpreadGooToCell.ToString("0.000")));
+            options.Gap();
 
-        options.CheckboxLabeled("MSS_GG_InfectOnGooTouch".Translate(), ref InfectOnGooTouch);
-        options.Gap();
+            ChanceForGooToDamagePercent = Widgets.HorizontalSlider(options.GetRect(40f), ChanceForGooToDamagePercent, 0f, 0.1f,
+                label: "MSS_GG_Setting_ChanceForGooToDamagePercent".Translate(ChanceForGooToDamagePercent.ToString("0.000")));
+            options.Gap();
 
-        options.Label("MSS_GG_Setting_MapGooReevaluateFrequency".Translate(MapGooReevaluateFrequency));
-        options.IntAdjuster(ref MapGooReevaluateFrequency, 60);
+            Widgets.FloatRange(options.GetRect(40), 1, ref GooDamageRange, 0f, 10f, "MSS_GG_Setting_GooDamageRange");
+            options.Gap();
 
-        options.Gap();
-        options.Gap();
+            options.CheckboxLabeled("MSS_GG_InfectOnGooTouch".Translate(), ref InfectOnGooTouch);
+            options.Gap();
 
-        options.Label("MSS_GG_Settings_ChanceToMerge".Translate((ChanceToMerge * 100f).ToString("0.0000")));
-        ChanceToMerge = options.Slider( ChanceToMerge, 0.0001f, 1f);
+            options.Label("MSS_GG_Setting_MapGooReevaluateFrequency".Translate(MapGooReevaluateFrequency));
+            options.IntAdjuster(ref MapGooReevaluateFrequency, 60);
 
-        options.Label("MSS_GG_Settings_ShamblerMergeHediffSeverityToTransform".Translate(ShamblerMergeHediffSeverityToTransform));
-        options.IntAdjuster(ref ShamblerMergeHediffSeverityToTransform, 1);
+            options.Gap();
 
-        options.Label("MSS_GG_Settings_MaxShamblersOnMap".Translate(MaxShamblersOnMap));
-        options.IntAdjuster(ref MaxShamblersOnMap, 1);
+            options.Label("MSS_GG_Settings_ChanceToMerge".Translate((ChanceToMerge * 100f).ToString("0.0000")));
+            ChanceToMerge = options.Slider(ChanceToMerge, 0.0001f, 1f);
 
-        options.Gap();
+            options.Label("MSS_GG_Settings_ShamblerMergeHediffSeverityToTransform".Translate(ShamblerMergeHediffSeverityToTransform));
+            options.IntAdjuster(ref ShamblerMergeHediffSeverityToTransform, 1);
 
-        options.End();
+            options.Label("MSS_GG_Settings_MaxShamblersOnMap".Translate(MaxShamblersOnMap));
+            options.IntAdjuster(ref MaxShamblersOnMap, 1);
+
+            options.Gap();
+        }
+        finally
+        {
+            options.End();
+            GUI.EndScrollView();
+        }
     }
 
     public override void ExposeData()
