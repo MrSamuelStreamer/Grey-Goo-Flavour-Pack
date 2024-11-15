3566cb8307d78196ff03f46a2936fbb0d566fb59
Kieran David Evans <keyz182@gmail.com>
2024-11-15 15:16:31 +0000

    Fix EMP turret targetting

---
+++ 1.5/Source/Grey_Goo/HarmonyPatches/VerbUtility_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Grey_Goo.HarmonyPatches;
+
+[HarmonyPatch(typeof(VerbUtility))]
+public static class VerbUtility_Patch
+{
+    [HarmonyPatch(nameof(VerbUtility.IsEMP))]
+    [HarmonyPostfix]
+    public static void IsEMP(Verb verb, ref bool __result)
+    {
+        if (verb.caster.def == Grey_GooDefOf.MSS_GG_Turret_EMPMiniTurret)
+        {
+            __result = false;
+        }
+    }
+}
